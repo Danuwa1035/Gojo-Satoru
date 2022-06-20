63bf04011ae7604e78b72a1ab5f81eb1a7e6cbf8
Danuwa1035 <107876279+Danuwa1035@users.noreply.github.com>
2022-06-20 22:23:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94762554100']
+global.premium = ['94762554100']
+global.ownernomer = 94762554100'
+global.ownername = '😉danuwa'
+global.botname = 'Danuwabot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'danuwabot'
+global.author = '😉danuwabot'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','😉','🙂','😜']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
